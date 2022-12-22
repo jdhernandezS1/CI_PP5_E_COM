6c078fabbcede535ecdb0f898666697d0429dd94
David_Hernandez <julio-david@hotmail.com>
2022-12-22 14:40:46 +0000

    Category model

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,22 @@
+"""
+Imports
+"""
+# 3rd party:
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 from django.contrib import admin
-
+# from django_summernote.admin import SummernoteModelAdmin
+# Internal
 # Register your models here.
+from .models import Cat
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+@admin.register(Cat)
+class CatAdmin(admin.ModelAdmin):
+    """
+    Post model class For posts in Blog
+    """
+    list_display = ('title', 'slug', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title']
+    list_filter = ('title', 'created_on')
--- products/models.py
+++ products/models.py
@@ -1,3 +1,61 @@
+"""
+Imports
+"""
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+# Internal
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Create your models here.
+
+class Cat(models.Model):
+    """
+    A class for Category product model
+    """
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="prod"
+        )
+    title = models.CharField(
+        max_length=200,
+        unique=True
+        )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True
+        )
+    featured_image = CloudinaryField(
+        'image',
+        default='placeholder'
+        )
+    created_on = models.DateTimeField(
+        auto_now_add=True
+        )
+
+    class Meta:
+        """
+        Items Order
+        """
+        ordering = [
+            '-created_on'
+            ]
+
+    def __str__(self):
+        """
+        Return Title
+        """
+        return str(self.title)
+
+
+# class product(models.Model):
+#     """
+#     A class for product model
+#     """
+#     content = models.TextField()
+#     featured_image = CloudinaryField(
+#         'image',
+#         default='placeholder'
+#         )
