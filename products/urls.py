fb39c624ca6dcb06b1e90a4a70c1af2e1114de7e
David_Hernandez <julio-david@hotmail.com>
2022-12-22 13:19:22 +0000

    Create Products Aplication

---
+++ products/urls.py
@@ -0,0 +1,12 @@
+# Imports
+# 3rd party:
+from django.urls import path, include
+from django.contrib import admin
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Internal
+from . import views
+# ~~~~~~~~~~
+
+urlpatterns = [
+    # path('', views.index, name='home'),
+]
