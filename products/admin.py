7044a272f39c5bbc7b33ab1681a976db929e921f
David_Hernandez <julio-david@hotmail.com>
2022-12-22 18:45:22 +0000

    setting Product model

--- products/admin.py
+++ products/admin.py
@@ -4,10 +4,10 @@ Imports
 # 3rd party:
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 from django.contrib import admin
-# from django_summernote.admin import SummernoteModelAdmin
+from django_summernote.admin import SummernoteModelAdmin
 # Internal
 # Register your models here.
-from .models import Cat
+from .models import Cat,  Prod
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
@@ -31,3 +31,30 @@ class CatAdmin(admin.ModelAdmin):
         'title',
         'created_on'
         )
+
+
+@admin.register(Prod)
+class ProdAdmin(SummernoteModelAdmin):
+    """
+    Post model class For Product
+    """
+    list_display = (
+        'title',
+        'title_slug',
+        'scount'
+        )
+    prepopulated_fields = {
+        'title_slug': ('title',)
+        }
+    search_fields = [
+        'title',
+        'title_slug'
+
+        ]
+    list_filter = (
+        'title',
+        'title_slug',
+        'scount'
+        )
+
+    summernote_filds = ('description')
--- products/models.py
+++ products/models.py
@@ -19,18 +19,22 @@ class Cat(models.Model):
         on_delete=models.CASCADE,
         related_name="prod"
         )
+
     title = models.CharField(
         max_length=200,
         unique=True
         )
+
     slug = models.SlugField(
         max_length=200,
         unique=True
         )
+
     featured_image = CloudinaryField(
         'image',
         default='placeholder'
         )
+
     created_on = models.DateTimeField(
         auto_now_add=True
         )
@@ -50,12 +54,67 @@ class Cat(models.Model):
         return str(self.title)
 
 
-# class product(models.Model):
-#     """
-#     A class for product model
-#     """
-#     content = models.TextField()
-#     featured_image = CloudinaryField(
-#         'image',
-#         default='placeholder'
-#         )
+class Prod(models.Model):
+    """
+    A class for product model
+    """
+    category = models.ForeignKey(
+        Cat,
+        on_delete=models.CASCADE,
+        related_name="category"
+    )
+
+    title = models.CharField(
+        max_length=200,
+        unique=True
+        )
+
+    title_slug = models.SlugField(
+        max_length=200,
+        unique=True
+        )
+
+    price = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        default=0
+        )
+
+    scount = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        default=0
+        )
+
+    quantity = models.IntegerField(
+        default=0
+        )
+
+    featured_image = CloudinaryField(
+        'image',
+        default='placeholder'
+        )
+
+    scountbool = models.BooleanField()
+
+    description = models.TextField()
+
+    created_on = models.DateTimeField(
+        auto_now_add=True
+        )
+
+    class Meta:
+        """
+        Items Order
+        """
+        ordering = [
+            '-created_on',
+            'category',
+            'title',
+            ]
+
+    def __str__(self):
+        """
+        Return Title
+        """
+        return str(self.title)
