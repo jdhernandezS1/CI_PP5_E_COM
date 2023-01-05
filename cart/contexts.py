b67fe465bda6d009cb889936598023656554bb0a
David_Hernandez <julio-david@hotmail.com>
2023-01-05 17:49:03 +0000

    Cart Table View Template and context

--- cart/contexts.py
+++ cart/contexts.py
@@ -2,8 +2,10 @@
 # 3rd party:
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Internal
+# Internal]
+from products.models import Prod
 # ~~~~~~~~~~
 
 
@@ -12,7 +14,19 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
-
+    cart = request.session.get('cart', {})
+    
+    for prod_id, quantity in cart.items():
+        product = get_object_or_404(Prod, pk=prod_id)
+        total += quantity * product.price
+        product_count += quantity
+        temp_tot = product.price * quantity
+        cart_items.append({
+            'prod_id': prod_id,
+            'quantity': quantity,
+            'product': product,
+            'temp_tot': temp_tot,
+        })
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
