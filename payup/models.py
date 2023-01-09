ea90c870a0dc22c7c7e46aa0830864cb4612a40d
David_Hernandez <julio-david@hotmail.com>
2023-01-09 14:50:31 +0000

    create Order models and internal products

--- payup/models.py
+++ payup/models.py
@@ -5,6 +5,32 @@ Imports
 # 3rd party:
 from django.db import models
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
 # Internal
+from products.models import Prod
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=12, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=80, null=False, blank=False)
+    phone_number = models.CharField(max_length=11, null=False, blank=False)
+    canton = models.CharField(max_length=40, null=False, blank=False)
+    city = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=6, null=True, blank=True)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+
+class OrdProd(models.Model):
+    """
+    Products in the order
+    """
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='prodsorder')
+    product = models.ForeignKey(Prod, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    prods_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
