7dfce29787de8ed13be773b99a67e3df5e4e1baf
David_Hernandez <julio-david@hotmail.com>
2023-01-09 16:32:08 +0000

    Conecting signals functions and app

--- payup/apps.py
+++ payup/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PayupConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'payup'
+
+    def ready(self):
+        import payup.signals
