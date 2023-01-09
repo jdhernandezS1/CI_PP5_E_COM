419a7a52640c0e088e81e634266535962dd89b22
David_Hernandez <julio-david@hotmail.com>
2023-01-09 20:21:31 +0000

    Fix Space Error

--- payup/forms.py
+++ payup/forms.py
@@ -49,4 +49,3 @@ class OrderForm(forms.ModelForm):
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = " "
-
