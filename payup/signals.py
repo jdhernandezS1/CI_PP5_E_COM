5cfe350bda02798bc1ff589f088ece16e86b32b9
David_Hernandez <julio-david@hotmail.com>
2023-01-09 16:33:17 +0000

    Conecting signals functions and app

---
+++ payup/signals.py
@@ -0,0 +1,25 @@
+"""
+Imports
+"""
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+# Internal
+from .models import OrdProd
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+@receiver(post_save, sender=OrdProd)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update order total on product update/create
+    """
+    instance.order.update_total()
+
+@receiver(post_delete, sender=OrdProd)
+def update_on_save(sender, instance, **kwargs):
+    """
+    Update order total on lineitem delete
+    """
+    instance.order.update_total()
