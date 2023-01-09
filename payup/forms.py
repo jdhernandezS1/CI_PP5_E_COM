f80875e9d9072dfb5b14c1feeca145b7181f57f0
David_Hernandez <julio-david@hotmail.com>
2023-01-09 18:02:36 +0000

    Create form for Checkout template

---
+++ payup/forms.py
@@ -0,0 +1,50 @@
+"""
+Imports
+"""
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django import forms
+# Internal
+from .models import Order
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = (
+            'full_name',
+            'email',
+            'phone_number',
+            'street_address1',
+            'street_address2',
+            'city',
+            'postcode',
+            'canton',
+            )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Placeholders classes labels
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email',
+            'phone_number': 'Phone number',
+            'canton': 'Canton',
+            'city': 'City',
+            'postcode': 'NPA',
+            'street_address1': 'Address',
+            'street_address2': 'Extra Address',
+            }
+        # autofocus the first field
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
