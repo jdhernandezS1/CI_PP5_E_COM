484e314b3f9c6e67b4c3fb99e7cc9530497396ed
David_Hernandez <julio-david@hotmail.com>
2023-01-09 16:31:34 +0000

    Setting Admin view

--- payup/admin.py
+++ payup/admin.py
@@ -1,3 +1,63 @@
+"""
+Imports
+"""
+# 3rd party:
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 from django.contrib import admin
+# Internal
+from .models import Order, OrdProd
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Register your models here.
+
+class OrderProds(admin.TabularInline):
+    model = OrdProd
+    readonly_fields = (
+        'prods_total',
+        )
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (
+        OrderProds,
+        )
+
+    readonly_fields = (
+        'order_number',
+        'date',
+        'delivery_cost',
+        'order_total',
+        'grand_total',
+        )
+
+    fields = (
+        'order_number',
+        'full_name',
+        'email',
+        'phone_number',
+        'canton',
+        'city',
+        'postcode',
+        'street_address1',
+        'street_address2',
+        'date',
+        'delivery_cost',
+        'order_total',
+        'grand_total',
+        )
+
+    list_display = (
+        'order_number',
+        'date',
+        'full_name',
+        'order_total',
+        'delivery_cost',
+        'grand_total',
+        )
+
+    ordering = (
+        '-date',
+        '-order_number',
+        )
+
+
+admin.site.register(Order, OrderAdmin)
