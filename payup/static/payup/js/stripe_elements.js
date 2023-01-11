85daf38377007cb4d5e25de2cbc3c7415b722e43
David_Hernandez <julio-david@hotmail.com>
2023-01-11 17:43:28 +0000

    Fixing Card Division and setting style

--- payup/static/payup/js/stripe_elements.js
+++ payup/static/payup/js/stripe_elements.js
@@ -6,23 +6,22 @@ See your keys here: https://dashboard.stripe.com/apikeys
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
-console.log(stripe.elements);
 
-// var elements = stripe.elements();
-// var style = {
-//     base: {
-//         color: '#000',
-//         fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-//         fontSmoothing: 'antialiased',
-//         fontSize: '16px',
-//         '::placeholder': {
-//             color: '#aab7c4'
-//         }
-//     },
-//     invalid: {
-//         color: '#dc3545',
-//         iconColor: '#dc3545'
-//     }
-// };
-// var card = elements.create('card', {style: style});
-// card.mount('#card-element');
+var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
