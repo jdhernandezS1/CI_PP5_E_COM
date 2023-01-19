71c8946b03e207c754d807421b6119bf8e1a2b6e
David_Hernandez <julio-david@hotmail.com>
2023-01-19 15:23:16 +0000

    Add Super User requirement to manage the store

--- manager/views.py
+++ manager/views.py
@@ -18,19 +18,38 @@ def ProductManager(request):
     """
     Get Products Function
     """
-    prod_list = Prod.objects.all()
-    categories = Cat.objects.all()
-    template = "manager/prods_manager.html"
-    if request.GET:
-        orders = 'title'
-        queryset = prod_list.order_by(orders)
-        prod_list = get_list_or_404(queryset)
-    context = {
-        "prod_list": prod_list,
-        "categories": categories
-        }
-    return render(
-        request,
-        template,
-        context
-        )
+    if request.user.is_superuser:
+        prod_list = Prod.objects.all()
+        categories = Cat.objects.all()
+        template = "manager/prods_manager.html"
+        if request.GET:
+            orders = 'title'
+            queryset = prod_list.order_by(orders)
+            prod_list = get_list_or_404(queryset)
+        context = {
+            "prod_list": prod_list,
+            "categories": categories
+            }
+        return render(
+            request,
+            template,
+            context
+            )
+    else: 
+        return redirect("home")
+
+
+class DelItem(generic.ListView):
+    """
+    A clas To Delete Item
+    """
+    model = Prod
+
+    def post(self, request, title_slug, productid, **kwargs):
+        """
+        method To Delete Item
+        """
+        product = get_object_or_404(model, id=productid)
+        product.delete()
+        messages.success(request, 'The Product Was Deleted as well')
+        return redirect('products_manager', title_slug)
