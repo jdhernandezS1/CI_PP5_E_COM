0003e25ace746b76a1f6a66df6c6f345ca19adb1
David_Hernandez <julio-david@hotmail.com>
2023-01-25 17:51:25 +0000

    Setting Email Confirmation

--- e_comm/settings.py
+++ e_comm/settings.py
@@ -240,3 +240,17 @@ STANDARD_DELIVERY_PERCENTAGE = 20
 STRIPE_CURRENCY = "chf"
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
+
+# Gmail Credentials
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'store.capricci.ch@gmail.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
