44d194429b4943156d04775fbc992dd270fadfb9
David_Hernandez <julio-david@hotmail.com>
2023-01-27 17:06:42 +0000

    errors Views Rendering

--- errors/views.py
+++ errors/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.views import generic, View
 
-# Create your views here.
+from products.models import Cat
+
+
+class ServerDeniesAcces(View):
+    """
+    Page not found Error 403
+    """
+    def get(self, request, *args, **kwargs):
+        template = 'errors/403.html'
+        return render(request, template)
+
+
+class PageNotFound(View):
+    """
+    Page not found Error 404
+    """
+    def get(self, request, *args, **kwargs):
+        template = 'errors/404.html'
+        return render(request, template)
+
+
+class InternalServerError(View):
+    """
+    Page not found Error 500
+    """
+    def get(self, request, *args, **kwargs):
+        template = 'errors/500.html'
+        return render(request, template)
