dc24de832daf4ff872cfff7903e04a7647ed9e4d
David_Hernandez <julio-david@hotmail.com>
2023-02-15 16:37:04 +0000

    Finish Application Details

--- userprofile/views.py
+++ userprofile/views.py
@@ -4,10 +4,8 @@ Imports
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 3rd party:
 from django.shortcuts import render, get_object_or_404
-from django.shortcuts import reverse, get_list_or_404, redirect
-from django.db.models import Q
+from django.shortcuts import reverse, get_list_or_404
 from django.views import generic, View
-from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.core.exceptions import ValidationError
 # Internal
