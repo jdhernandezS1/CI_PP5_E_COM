108e06280239dfda11a466bf5dfe241e746b8ef9
David_Hernandez <julio-david@hotmail.com>
2023-02-24 16:48:55 +0000

    Templates and configuration

--- e_comm/settings.py
+++ e_comm/settings.py
@@ -32,8 +32,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-# DEBUG = True
+# DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = [
     'www.capricci.herokuapp.com',
     'https://www.capricci.herokuapp.com',
