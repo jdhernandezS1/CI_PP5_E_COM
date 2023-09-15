8ebdf1e82f80a6a5180e4498fe618cf5a97517e1
David <julio-david@hotmail.com>
2023-09-15 23:14:40 +0200

    products details

--- front/src/Pages/Products/Products.jsx
+++ front/src/Pages/Products/Products.jsx
@@ -1,5 +1,67 @@
-const Products = () => {
-    return <h1>Products</h1>;
-  };
-  
-  export default Products;
+import React, { Component } from 'react';
+import { fetchDataFromApi } from '../../Services/Utils//httpClient'; // Import the reusable function
+import styles from '../../Assets/Styles/Products.module.scss'
+
+class Products extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      responseData: null,
+      error: null,
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      // Use the fetchDataFromApi function to make a GET request
+      const data = await fetchDataFromApi('products/'); // Replace with your specific path
+      this.setState({ responseData: data });
+    } catch (error) {
+      this.setState({ error: error });
+    }
+  }
+
+  render() {
+    const { responseData, error } = this.state;
+    if (error) {
+
+      return (
+        <div>Error: {error.message}</div>
+      );
+    }
+
+    if (!responseData) {
+      return <div>Loading...</div>;
+    }
+
+    // Render the component with the received data
+    return (
+      <div>
+        <h1>Stock:</h1>
+        <ul className={styles.ProductsGrid}>
+            {responseData.map((item) => (
+              <li key={item.id} className={styles.ProductCard}>
+                {/* {item.category} */}
+                {/* {item.price} */}
+                {/* {item.scount} */}
+                <div>
+                  <picture className={styles.ProductPicture}>
+                    <img src={`https://res.cloudinary.com/djvwk7zf2/${item.featured_image}`} alt={item.slug_title}  className={styles.ProductImage}/>
+                  </picture>
+                </div>
+                <div className={styles.ProductDescription}>
+                  {item.title}
+                <p>Remains: {item.quantity}</p>
+                <p>{item.description}</p>
+                </div>
+                {/* {item.scountbool} */}
+
+              </li>
+            ))}
+        </ul>
+      </div >
+    );
+  }
+}
+export default Products;
