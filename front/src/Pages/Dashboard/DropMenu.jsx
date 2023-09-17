1d11ce8296a0004745b6552acb2aba079d9d2633
David <julio-david@hotmail.com>
2023-09-17 15:59:01 +0200

    Feature Drop Menu Small Screen

--- front/src/Pages/Dashboard/DropMenu.jsx
+++ front/src/Pages/Dashboard/DropMenu.jsx
@@ -1,40 +1,39 @@
-import React, { Component } from 'react';
-import styles from '../../Assets/Styles/NavigationBar.module.scss'
-import { Outlet, Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import styles from '../../Assets/Styles/NavigationBar.module.scss';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaHome } from 'react-icons/fa';
+import FloatingButtons from 'react-floating-buttons';
 
-class DropdownMenu extends Component {
-  constructor(props) {
-    super(props);
+function DropdownMenu(props) {
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
 
-    this.state = {
-      isOpen: false,
-    };
-  }
+  const buttonsList = [
+    { onClick: () => { navigate('/'); }, text: ' Home', },
+    { onClick: () => { navigate('/products'); }, text: ' Home', icon: <FaHome />, },
+  ];
 
-  toggleMenu = () => {
-    this.setState((prevState) => ({
-      isOpen: !prevState.isOpen,
-    }));
+  const toggleMenu = () => {
+    setIsOpen((prevIsOpen) => !prevIsOpen);
   };
 
-  render() {
-    const { isOpen } = this.state;
-
-    return (
-      <div >
-        <button className={styles.DropdownMenuButton} onClick={this.toggleMenu}>
-          {this.props.triggerText}
-        </button>
-        {isOpen && (
-          <ul className={styles.DropdownMenu}>
-            <li ><Link to="/">Home</Link></li>
-            <li ><Link to="/Products/">Products</Link></li>
-            <li ><Link to="/">Home</Link></li>
-          </ul>
-        )}
+  return (
+      <div className={styles.DropdownMenuTitle}>
+        <div className={styles.DropdownMenuButton}><FloatingButtons
+          onClick={toggleMenu}
+          // buttonType="plus"
+          variant="plus"
+          dimension={50}
+          buttonsList={buttonsList}
+          top={'calc(50% - 25px)'}
+          left={'5%'}
+          direction="right"
+        >
+          Capriccio
+        </FloatingButtons></div>
+      <div><h1>Capriccio</h1></div>
       </div>
-    );
-  }
+  );
 }
 
 export default DropdownMenu;
