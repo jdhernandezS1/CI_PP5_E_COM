e564fa38fe4119f5304ab954053cf83b3d87f82a
David Hernandez <48605281+jdhernandezS1@users.noreply.github.com>
2024-12-15 15:55:53 +0000

    HOSTS

--- e_comm/settings.py
+++ e_comm/settings.py
@@ -34,12 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = [
-    'www.capricci.herokuapp.com',
-    'https://www.capricci.herokuapp.com',
-    'https://capricci.herokuapp.com',
-    'capricci.herokuapp.com',
-    '127.0.0.1',
-    'localhost']
+    '*']
 
 # Application definition
 
